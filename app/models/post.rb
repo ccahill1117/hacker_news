6429eeb5e592f4d88011656490fcaece2af7cb32
christopher cahill and Robert Bruce <robert@>
2018-12-11 15:08:35 -0800

    add time duration for posts since they were created

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   has_many :comments
   validates :description, :url, :presence => true
+
+  def since
+    (Time.current - self.created_at)/60
+  end
 end
